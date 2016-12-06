ee59490ccb384b4b5868500e7713fbe015a7f88b
PeteGH <peter@grant-hay.eu>
2016-12-06 20:55:53 +0100

    adding 8:10

--- directory.rb
+++ directory.rb
@@ -5,12 +5,12 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.delete "\n"
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+      puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
@@ -43,10 +43,10 @@ def short_name(students)
     short.each_with_index do |student, i|
       puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
-  end
+end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
